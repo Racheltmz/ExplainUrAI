dc1d6debe026eee5c6374336a9128e4d5ce1c1d9
Rachel Tan <racheltanmz@gmail.com>
2023-06-04 11:51:28 +0800

    Updated frontend application

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 # Import libraries
-from fastapi import FastAPI, Form, File, UploadFile
-from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile
 from tensorflow.keras.models import load_model
 import shutil
 from PIL import Image
@@ -13,36 +12,8 @@ from CLTasks import CLTasks
 # Initialise FastAPI class
 app = FastAPI()
 
-# Task: Can use for form fields, error handling: ValidationError
-class DLTextReq(BaseModel):
-    model: UploadFile
-    text: str
-    classes: list
-    num_features: int
-    num_samples: int = 1000 
-    need_process: bool = False
-    ds: UploadFile
-    ds_text_field: str = None
-    max_num: int = None
-    max_seq: int = None
-
-class CLReq(BaseModel):
-    model: UploadFile
-    X_test: UploadFile
-    y_test: UploadFile
-    selected_all: bool
-    selected_graphs: list = None
-    target_names: list = None
-    sample_indexes: list = None
-    scoring: str = 'balanced_accuracy' 
-    top_features: int = 5
-    feature: str = None
-    dist_graph: str = 'boself.sh_expot'
-    local_max_features: int = 3
-    compacity_nb_features: int = 5
-    xai_type: str = 'shap'
-
 # APIs get reports for XAI tasks
+# Deep Learning Image Classification
 @app.post("/dlimage")
 def explain_dlimage(model: UploadFile, imgs: list[UploadFile]):
     # Save the uploaded file
@@ -63,23 +34,61 @@ def explain_dlimage(model: UploadFile, imgs: list[UploadFile]):
     report_name = dl_img_xai.generate_report()
     return {'filename': report_name}
 
+# Deep Learning Binary Text Classification
 @app.post("/dltextbin")
-def explain_dltextbinary(req: DLTextReq):
-    dl_textbin_xai = DLTextBinary(req.model, req.text, req.classes, req.num_features, req.num_samples, 
-                                  req.need_process, req.ds, req.ds_text_field, req.max_num, req.max_seq)
+def explain_dltextbinary(
+    model: UploadFile, 
+    text: str,
+    classes: list,
+    num_features: int,
+    num_samples: int = 1000,
+    need_process: bool = False,
+    ds: UploadFile = None,
+    ds_text_field: str = None,
+    max_num: int = None,
+    max_seq: int = None):
+    dl_textbin_xai = DLTextBinary(model, text, classes, num_features, num_samples, 
+                                  need_process, ds, ds_text_field, max_num, max_seq)
     report_name = dl_textbin_xai.generate_report()
     return {'filename': report_name}
 
+# Deep Learning Multi Text Classification (multi-class/multi-label)
 @app.post("/dltextmulti")
-def explain_dltextmulti(req: DLTextReq):
-    dl_textmulti_xai = DLTextMulti(req.model, req.text, req.classes, req.num_features, req.num_samples, 
-                                  req.need_process, req.ds, req.ds_text_field, req.max_num, req.max_seq)
+def explain_dltextmulti(
+    model: UploadFile, 
+    text: str,
+    classes: list,
+    num_features: int,
+    num_samples: int = 1000,
+    need_process: bool = False,
+    ds: UploadFile = None,
+    ds_text_field: str = None,
+    max_num: int = None,
+    max_seq: int = None):
+    dl_textmulti_xai = DLTextMulti(model, text, classes, num_features, num_samples, 
+                                  need_process, ds, ds_text_field, max_num, max_seq)
     report_name = dl_textmulti_xai.generate_report()
     return {'filename': report_name}
 
+# Classical Learning tasks
 @app.post("/cltask/{task}")
-def explain_cltask(req: CLReq, task: str):
-    cl_xai = CLTasks(task, req.model, req.X_test, req.y_test, req.selected_all, req.selected_graphs, req.target_names, req.sample_indexes, req.scoring, 
-                    req.top_features, req.feature, req.dist_graph, req.local_max_features, req.compacity_nb_features, req.xai_type)
+def explain_cltask(
+    task: str, 
+    model: UploadFile, 
+    X_test: UploadFile, 
+    y_test: UploadFile,
+    selected_all: bool,
+    selected_graphs: list = None,
+    target_names: list = None,
+    sample_indexes: list = None,
+    scoring: str = 'balanced_accuracy' ,
+    top_features: int = 5,
+    feature: str = None,
+    dist_graph: str = 'boself.sh_expot',
+    local_max_features: int = 3,
+    compacity_nb_features: int = 5,
+    xai_type: str = 'shap'):
+    cl_xai = CLTasks(task, model, X_test, y_test, selected_all, selected_graphs, target_names, sample_indexes, scoring, 
+                    top_features, feature, dist_graph, local_max_features, compacity_nb_features, xai_type)
     report_name = cl_xai.generate_report()
     return {'filename': report_name}
