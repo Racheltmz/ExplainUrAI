89b253ce4f9ee8bce079d58ca42944cba42ab65f
Rachel Tan <racheltanmz@gmail.com>
2023-06-30 10:13:41 +0800

    Hide test files and create folder if doesn't exist

--- scripts/xai_base.py
+++ scripts/xai_base.py
@@ -1,11 +1,14 @@
 # Import Libraries
+import os
 from datetime import datetime
 
 class XAI():
     def __init__(self):
-        self.report_convention = '../reports/explainurai_'
+        self.report_dir = '../reports'
+        self.report_convention = f'{self.report_dir}/explainurai_'
         self.current_epoch = int(datetime.now().timestamp()) # Current datetime in epoch
         self.font = 'Bahnschrift'
+        self.create_dir_if_notexists()
 
     @property
     def get_report_convention(self):
@@ -18,3 +21,6 @@ class XAI():
     @property
     def get_matplotlib_font(self):
         return self.font
+
+    def create_dir_if_notexists(self):
+        os.mkdir(self.report_dir)
